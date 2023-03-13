c6ec0b6caaea4816cac9c8a85749661e746b659b
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-03-13 22:41:41 +1100

    a

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -38,16 +38,16 @@ function Skills() {
 
             </p>
             <MailchimpSubscribe
-    url={url}
-    render={({ subscribe, status, message }) => (
-      <div>
-        <SimpleForm onSubmitted={formData => subscribe(formData)} />
-        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
-        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
-      </div>
-    )}
-  />
+              url={url}
+              render={({ subscribe, status, message }) => (
+                <div>
+                  <SimpleForm onSubmitted={formData => subscribe(formData)} />
+                  {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+                  {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+                  {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+                </div>
+              )}
+            />
 
 
           </div>
