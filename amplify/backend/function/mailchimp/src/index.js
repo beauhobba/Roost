45884412ee77088b5a90b9aa7d21fa61273350d4
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-03-15 17:42:10 +1100

    Added: AWS secret

--- amplify/backend/function/mailchimp/src/index.js
+++ amplify/backend/function/mailchimp/src/index.js
@@ -5,6 +5,7 @@
  * 
  * const key = "af5fd7622e09b69a3f30fd64f3778930-us14"
  */
+const AWS = require('aws-sdk');const secretsManager = new AWS.SecretsManager();
 const mailchimp_key =  process.env.MAILCHIMP_KEY
 const server_prefix = "us14"
 const mailchimp = require("@mailchimp/mailchimp_marketing");
@@ -21,7 +22,12 @@ exports.handler = async (event) => {
   const { firstName, lastName, email } = JSON.parse(event.body);
   console.log("firstname: "+ firstName)
   console.log("lastname: "+lastName)
-  console.log("key: "+mailchimp_key)
+  console.log("lastname: "+email)
+  // console.log("key: "+mailchimp_key)
+  // console.log(process.env)
+  const secretData = await secretsManager .getSecretValue({ SecretId: 'MailchimpRoost' }) .promise(); 
+  const secretValues = JSON.parse(secretData.SecretString); 
+  console.log('DEMO_API_KEY', secretValues.DEMO_API_KEY);
 
     const response = await mailchimp.lists.addListMember(listId, {
         email_address: email,
