6b5651653d3d6c1bbb21caed3a138faaf32e9cf8
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-03-25 19:54:01 +1100

    Added: Roost stuff

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import coverphoto from "../images/coverphoto.png";
 import { API } from 'aws-amplify';
-import { getPosts } from '../graphql/queries';
+import { listTodos } from '../graphql/queries';
 
 function Blog() {
   const [posts, setPosts] = useState([]);
@@ -12,11 +12,11 @@ function Blog() {
 
   async function fetchPosts() {
     try {
-      const postData = await API.graphql({ query: getPosts });
+      const postData = await API.graphql({ query: listTodos });
       console.log("yo");
-      console.log(postData.data.getPosts.items);
+      console.log(postData.data.listTodos.items);
       console.log("yo");
-      setPosts(postData.data.getPosts.items);
+      setPosts(postData.data.listTodos.items);
     } catch (error) {
       console.log('Error fetching posts', error);
     }
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,12 +1,45 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createPost = /* GraphQL */ `
-  mutation CreatePost($title: String!, $content: String!) {
-    createPost(title: $title, content: $content) {
+export const createTodo = /* GraphQL */ `
+  mutation CreateTodo(
+    $input: CreateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    createTodo(input: $input, condition: $condition) {
       id
-      title
-      content
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateTodo = /* GraphQL */ `
+  mutation UpdateTodo(
+    $input: UpdateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    updateTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteTodo = /* GraphQL */ `
+  mutation DeleteTodo(
+    $input: DeleteTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    deleteTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
     }
   }
 `;
