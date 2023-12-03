722b419b9e4426ca54dd398fca94fbd1df24b2b9
hobbabeau <82560357+hobbabeau@users.noreply.github.com>
2023-12-03 14:30:41 +1100

    Added: new details on the website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,6 @@ import { font_families, colours } from "./styles";
 const myAPI = "Contact";
 const otherAPI = "mailchimp";
 
-
 function Contact() {
   const [direction, setDirection] = useState("row");
   const [buttonFontSize, setButtonFontSize] = useState(20);
@@ -46,40 +45,42 @@ function Contact() {
     setDescription(event.target.value);
   }
 
-    //Function to fetch from our backend and update customers array
-    function addEmail(e) {
-      console.log('here')
-      if (email == "") {
-        setError("Please enter an email address");
-        return;
-      }
-      if (isChecked == true){
-        let data = { firstName: firstName, lastName: lastName, email: email };
-        API.post(otherAPI, "/mailchimp", { body: data })
-          .then((response) => {
-            console.log(response);
-            setError("Thankyou for joining the waitlist!");
-          })
-          .catch((error) => {
-            setError("There was an error with joining the waitlist");
-          });
-      }
-      
-
-
-      let data = { firstName: firstName, lastName: lastName, email: email, description: description, subject: subject };
-      API.post(myAPI, "/Contact", { body: data })
+  //Function to fetch from our backend and update customers array
+  function addEmail(e) {
+    console.log("here");
+    if (email == "") {
+      setError("Please enter an email address");
+      return;
+    }
+    if (isChecked == true) {
+      let data = { firstName: firstName, lastName: lastName, email: email };
+      API.post(otherAPI, "/mailchimp", { body: data })
         .then((response) => {
           console.log(response);
-          setError("Message Sent");
+          setError("Thankyou for joining the waitlist!");
         })
         .catch((error) => {
-          console.log('Error')
-          setError("There was an error with sending your message");
+          setError("There was an error with joining the waitlist");
         });
     }
 
-
+    let data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      description: description,
+      subject: subject,
+    };
+    API.post(myAPI, "/Contact", { body: data })
+      .then((response) => {
+        console.log(response);
+        setError("Message Sent");
+      })
+      .catch((error) => {
+        console.log("Error");
+        setError("There was an error with sending your message");
+      });
+  }
 
   useEffect(() => {
     if (isMobile) {
@@ -89,8 +90,6 @@ function Contact() {
       setSideImgSize("100%");
     }
   }, []);
-
-
 
   useEffect(() => {
     if (isMobile) {
@@ -109,13 +108,11 @@ function Contact() {
       </Helmet>
 
       <div>
-
         <div
           style={{
             display: "flex",
             flexDirection: direction,
             justifyContent: "center",
-
 
             fontFamily: font_families.roost,
           }}
@@ -136,107 +133,116 @@ function Contact() {
                   alt=""
                 />
               </div>
-              
             </div>
           )}
 
-
-<div  style={{width: "100%"}} >
-          <ContactScreen/>
-          <div style={{alignItems: 'flex-start', display: 'flex', flexDirection: 'column', paddingLeft: 30}}>
-            <p>
-              Need to get in touch, use the form below! 
-            </p>
-        <label style={{marginBottom: 0}}>
-              First Name:
-              <br></br>
-              <input
-                type="text"
-                value={firstName}
-                onChange={handleChangeFirstName}
-              />
-            </label>
-            <label style={{marginBottom: 0}}>
-              Last Name:
-              <br></br>
-              <input
-                type="text"
-                value={lastName}
-                onChange={handleChangeLastName}
-              />
-            </label>
-            <label>
-              <b>*</b>Email:
-              <br></br>
-              <input
-                type="text"
-                value={email}
-                onChange={handleChangeEmail}
-                style={{
-                  width: "100%",
-                }}
-              />
-            </label>
-            <label>
-              <b>*</b>Subject:
-              <br></br>
-              <input
-                type="text"
-                value={subject}
-                onChange={handleChangeSubject}
-                style={{
-                  width: "100%",
-                  height: 100
-                }}
-              />
-            </label>
-            <label>
-              <b>*</b>Description:
-              <br></br>
-              <input
-                type="text"
-                value={description}
-                onChange={handleChangeDescription}
-                style={{
-                  width: "100%",
-                  height: 100
-                }}
-              />
-            </label>
-            <label class="checkbox" for="newsletter">
-                    <input
-                      type="checkbox"
-                      checked={isChecked}
-                      onChange={handleCheckboxChange}
-                    />
-                    Do you want to sign up to the newsletter?
-                  </label>
-                  <br></br>
-            <p>
-              <i>{error}</i>
-            </p>
-
-                  <button
-              onClick={addEmail}
+          <div style={{ width: "100%" }}>
+            <ContactScreen />
+            <div
               style={{
-                color: "white",
-                backgroundColor: "#EC2383",
-                fontSize: 20,
-                height: 40,
-                width: "100%",
-                border: "none",
-                marginBottom: 20
+                alignItems: "flex-start",
+                display: "flex",
+                flexDirection: "column",
+                paddingLeft: 30,
               }}
             >
-              Send Message
-            </button>
+              <p>Need to get in touch, use the form below!</p>
+              <label style={{ marginBottom: 0, width: '60%', display: 'block' }}>
+                First Name:
+                <br></br>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={handleChangeFirstName}
+                  style={{ width: '100%', boxSizing: 'border-box' }}
+                />
+              </label>
+              <label style={{ marginBottom: 0, width: '60%', display: 'block' }}>
+                Last Name:
+                <br></br>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={handleChangeLastName}
+                  style={{ width: '100%', boxSizing: 'border-box' }}
+                />
+              </label>
+              <label style={{ marginBottom: 0, width: '60%', display: 'block' }}>
+                <b>*</b>Email:
+                <br></br>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={handleChangeEmail}
+                  style={{ width: '100%', boxSizing: 'border-box' }}
+                />
+              </label>
+              <label style={{ marginBottom: 0, width: '60%', display: 'block' }}>
+                <b>*</b>Subject:
+                <br></br>
+                <input
+                  type="text"
+                  value={subject}
+                  onChange={handleChangeSubject}
+                  style={{ width: '100%', boxSizing: 'border-box' }}
+                />
+              </label>
+              <label style={{ marginBottom: 0, width: '60%', display: 'block' }}>
+                <b>*</b>Description:
+                <br></br>
+                <textarea
+                  type="text"
+                  value={description}
+                  onChange={handleChangeDescription}
+                  style={{ width: '100%', boxSizing: 'border-box', height: 100 }}
+                />
+              </label>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <label
+                  class="checkbox"
+                  for="newsletter"
+                >
+                  <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={handleCheckboxChange}
+                  />
+                </label>
 
-            </div>
-            </div>
+                <p style={{ paddingLeft: 10 }}>
+                    Do you want to sign up to the newsletter?
+                  </p>
+              </div>
+              <br></br>
+              <p>
+                <i>{error}</i>
+              </p>
 
+              <button
+                onClick={addEmail}
+                style={{
+                  color: "white",
+                  backgroundColor: "#EC2383",
+                  fontSize: 20,
+                  height: 40,
+                  width: "100%",
+                  border: "none",
+                  marginBottom: 20,
+                }}
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
 
           {/* <Commitment style={{width: "50%"}}/> */}
-
         </div>
         <div
           style={{
@@ -247,9 +253,6 @@ function Contact() {
             justifyContent: "center",
           }}
         />
-
-
-
 
         <div
           style={{
